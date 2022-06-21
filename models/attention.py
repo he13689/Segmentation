872c4079dbbbe674ac336ed3d8fdccf05563b49f
he13689 <375004220@qq.com>
2022-06-21 17:39:04 +0800

    修改

--- models/attention.py
+++ models/attention.py
@@ -1,6 +1,12 @@
+import math
+
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.autograd import Function
+from torch.cuda.amp import custom_fwd
+from torch.nn.init import constant_, xavier_uniform_
+from mmcv.ops.multi_scale_deform_attn import MultiScaleDeformableAttention as MSDA
 
 
 def drop_path(x, drop_prob: float = 0., training: bool = False):
@@ -164,3 +170,88 @@ class AttentionBlock(nn.Module):
         return x
 
 
+class MSDeformAttnFunction(Function):
+    @staticmethod
+    @custom_fwd(cast_inputs=torch.float32)
+    def forward(ctx, value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights, im2col_step):
+        ctx.im2col_step = im2col_step
+        output = MSDA.ms_deform_attn_forward(value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights, ctx.im2col_step)
+        ctx.save_for_backward(value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights)
+        return output
+
+    def backward(ctx, grad_output):
+        value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights = ctx.saved_tensors
+        grad_value, grad_sampling_loc, grad_attn_weight = MSDA.ms_deform_attn_backward(value, value_spatial_shapes, value_level_start_index,
+                                                                                       sampling_locations, attention_weights, grad_output, ctx.im2col_step)
+
+        return grad_value, None, None, grad_sampling_loc, grad_attn_weight, None
+
+
+class MSDeformAttn(nn.Module):
+    def __init__(self, d_model=256, n_levels=4, n_heads=8, n_points=4, ratio=1.0):
+        super().__init__()
+        if d_model % n_heads != 0:
+            raise ValueError('d_model must be divisible by n_heads, '
+                             'but got {} and {}'.format(d_model, n_heads))
+        _d_per_head = d_model // n_heads
+
+        self.im2col_step = 64
+
+        self.d_model = d_model
+        self.n_levels = n_levels
+        self.n_heads = n_heads
+        self.n_points = n_points
+        self.ratio = ratio
+        self.sampling_offsets = nn.Linear(d_model, n_heads * n_levels * n_points * 2)
+        self.attention_weights = nn.Linear(d_model, n_heads * n_levels * n_points)
+        self.value_proj = nn.Linear(d_model, int(d_model * ratio))
+        self.output_proj = nn.Linear(int(d_model * ratio), d_model)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        constant_(self.sampling_offsets.weight.data, 0.)
+        thetas = torch.arange(
+            self.n_heads, dtype=torch.float32) * (2.0 * math.pi / self.n_heads)
+        grid_init = torch.stack([thetas.cos(), thetas.sin()], -1)
+        grid_init = (grid_init / grid_init.abs().max(-1, keepdim=True)[0]).view(
+            self.n_heads, 1, 1, 2).repeat(1, self.n_levels, self.n_points, 1)
+        for i in range(self.n_points):
+            grid_init[:, :, i, :] *= i + 1
+
+        with torch.no_grad():
+            self.sampling_offsets.bias = nn.Parameter(grid_init.view(-1))
+        constant_(self.attention_weights.weight.data, 0.)
+        constant_(self.attention_weights.bias.data, 0.)
+        xavier_uniform_(self.value_proj.weight.data)
+        constant_(self.value_proj.bias.data, 0.)
+        xavier_uniform_(self.output_proj.weight.data)
+        constant_(self.output_proj.bias.data, 0.)
+
+    def forward(self, query, reference_points, input_flatten, input_spatial_shapes,
+                input_level_start_index, input_padding_mask=None):
+        N, Len_q, _ = query.shape
+        N, Len_in, _ = input_flatten.shape
+        assert (input_spatial_shapes[:, 0] * input_spatial_shapes[:, 1]).sum() == Len_in
+
+        value = self.value_proj(input_flatten)
+        if input_padding_mask is not None:
+            value = value.masked_fill(input_padding_mask[..., None], float(0))
+
+        value = value.view(N, Len_in, self.n_heads,
+                           int(self.ratio * self.d_model) // self.n_heads)
+        sampling_offsets = self.sampling_offsets(query).view(N, Len_q, self.n_heads, self.n_levels, self.n_points, 2)
+        attention_weights = self.attention_weights(query).view(N, Len_q, self.n_heads, self.n_levels * self.n_points)
+        attention_weights = F.softmax(attention_weights, -1).view(N, Len_q, self.n_heads, self.n_levels, self.n_points)
+
+        if reference_points.shape[-1] == 2:
+            offset_normalizer = torch.stack([input_spatial_shapes[..., 1], input_spatial_shapes[..., 0]], -1)
+            sampling_locations = reference_points[:, :, None, :, None, :] + sampling_offsets / offset_normalizer[None, None, None, :, None, :]
+        elif reference_points.shape[-1] == 4:
+            sampling_locations = reference_points[:, :, None, :, None, :2] \
+                                 + sampling_offsets / self.n_points * reference_points[:, :, None, :, None, 2:] * 0.5
+        else:
+            raise ValueError('Last dim of reference_points must be 2 or 4, but get {} instead.'.format(reference_points.shape[-1]))
+        output = MSDeformAttnFunction.apply(value, input_spatial_shapes, input_level_start_index, sampling_locations, attention_weights, self.im2col_step)
+        output = self.output_proj(output)
+        return output
--- models/vit.py
+++ models/vit.py
@@ -1,12 +1,13 @@
 # use the config of upernet_beit_adapter_large_480_80k_pascal_context_59_ss
+import math
 from functools import partial
 
 import torch
 import torch.nn as nn
 from loguru import logger
-from torch.nn.init import trunc_normal_
+from torch.nn.init import trunc_normal_, normal_
 
-from models.attention import AttentionBlock
+from models.attention import AttentionBlock, MSDeformAttn
 from models.embedding import HybridEmbed, PatchEmbed
 from utils.loader import load_checkpoint
 from utils.relative_position import RelativePosition
@@ -59,8 +60,8 @@ class BEIT(nn.Module):
         # 创建attention blocks, block的数量取决于depth深度
         self.blocks = nn.ModuleList(
             [AttentionBlock(dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale, drop=drop_rate,
-                   attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, with_cp=with_cp, init_values=init_values,
-                   window_size=self.patch_embed.patch_shape if use_rel_pos_bias else None) for i in range(depth)])
+                            attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, with_cp=with_cp, init_values=init_values,
+                            window_size=self.patch_embed.patch_shape if use_rel_pos_bias else None) for i in range(depth)])
 
         trunc_normal_(self.cls_token, std=.02)
 
@@ -88,9 +89,66 @@ class BEIT(nn.Module):
 
 
 # 基于 微软BEiT的网络
-class BackBone(BEIT):
+class SpatialPriorModule(nn.Module):
+    def __init__(self, inplanes=64, embed_dim=384):
+        super(SpatialPriorModule, self).__init__()
+
+
+class InteractionBlock:
     def __init__(self):
-        super(BackBone, self).__init__()
+        super(InteractionBlock, self).__init__()
+
+
+class BackBone(BEIT):
+    def __init__(self, pretrain_size=224, conv_inplane=64, n_points=4, deform_num_heads=6,
+                 init_values=0., cffn_ratio=0.25, deform_ratio=1.0, with_cffn=True, interaction_indexes=None, add_vit_feature=True, *args, **kwargs):
+        super(BackBone, self).__init__(init_values=init_values, *args, **kwargs)
+        self.interaction_indexes = interaction_indexes
+        self.add_vit_feature = add_vit_feature
+        self.pretrain_size = (pretrain_size, pretrain_size)
+        self.num_blocks = len(self.blocks)
+        self.flags = [i for i in range(-1, self.num_blocks, self.num_blocks // 4)][1:]
+
+        embed_dim = self.embed_dim  # =feature dim
+        self.level_embed = nn.Parameter(torch.zeros(3, embed_dim))
+
+        self.spm = SpatialPriorModule()
+
+        self.interactions = nn.Sequential(*[
+            InteractionBlock(dim=embed_dim, num_heads=deform_num_heads, n_points=n_points, init_values=init_values, drop_path=self.drop_path_rate,
+                             norm_layer=self.norm_layer, with_cffn=with_cffn, cffn_ratio=cffn_ratio, deform_ratio=deform_ratio,
+                             extra_extractor=True if i == len(interaction_indexes) - 1 else False) for i in range(len(interaction_indexes))])
+
+        self.up = nn.ConvTranspose2d(embed_dim, embed_dim, 2, 2)  # 放大2倍
+        self.norm1 = nn.SyncBatchNorm(embed_dim)
+        self.norm2 = nn.SyncBatchNorm(embed_dim)
+        self.norm3 = nn.SyncBatchNorm(embed_dim)
+        self.norm4 = nn.SyncBatchNorm(embed_dim)
+
+        self.up.apply(self._init_weights)
+        self.spm.apply(self._init_weights)
+        self.interactions.apply(self._init_weights)
+        self.apply(self._init_deform_weights)
+        normal_(self.level_embed)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm) or isinstance(m, nn.BatchNorm2d):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            fan_out //= m.groups
+            m.weight.data.normal_(0, math.sqrt(2.0 / fan_out))
+            if m.bias is not None:
+                m.bias.data.zero_()
+
+    def _init_deform_weights(self, m):
+        if isinstance(m, MSDeformAttn):
+            m._reset_parameters()
 
 
 if __name__ == '__main__':
